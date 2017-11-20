8bfe424e920ab86d40879cddde90bd09d34880e3
Rafael Angarita <angarita.rafael@gmail.com>
2017-11-20 13:32:39 +0100

    Add files via upload

---
+++ JdbcLab.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import oracle.jdbc.pool.OracleDataSource;
+
+public class JdbcLab {
+
+  private static Connection connection = null;
+  private static OracleDataSource ods = null;
+
+  public static void main(String...args) {
+
+    try  {
+      Class.forName ("oracle.jdbc.OracleDriver");
+    } catch(ClassNotFoundException e){
+      e.printStackTrace();
+    }
+
+    try {
+      ods = new OracleDataSource();
+      ods.setURL("jdbc:oracle:thin:@server:port:ORCL");
+      ods.setUser("username");
+      ods.setPassword("password");
+      connection = ods.getConnection();
+
+      System.out.println("Connected to the db!");
+
+      displayDepartment(connection);
+
+    } catch(SQLException e) {
+      e.printStackTrace();
+    } finally {
+      if(connection != null) {
+        try {
+          connection.close();
+        } catch(SQLException ignore) {
+          ignore.printStackTrace();
+        }
+      }
+    }
+  }
+
+  private static void displayDepartment(Connection connection) throws SQLException {
+    Statement statement = connection.createStatement();
+    ResultSet result = statement.executeQuery(
+      "SELECT deptno, dname FROM dept"
+    );
+    while(result.next()) {
+      int deptno = result.getInt("deptno");
+      String dname = result.getString("dname");
+      System.out.println("Department " + deptno + " has the name "
+        + dname);
+    }
+  }
+
+
+}
